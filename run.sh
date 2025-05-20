92a2fcca2d76f7e81331fd5d6a78b7f4c4049a46
torovtn <toribiovitonsconza@gmail.com>
2025-05-20 14:04:58 -0300

    Darwin kernel compatibility

--- run.sh
+++ run.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
-if [[ $1 = '-d' ]]; then
-  qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512 -audiodev pa,id=random_id -machine pcspk-audiodev=random_id -S -gdb tcp::1234 -d int
+# Determine OS
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  AUDIO_DRIVER="coreaudio"
 else
-  qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512 -audiodev pa,id=random_id -machine pcspk-audiodev=random_id
+  AUDIO_DRIVER="pa"
+fi
+
+if [[ $1 = '-d' ]]; then
+  qemu-system-x86_64 \
+    -hda Image/x64BareBonesImage.qcow2 \
+    -m 512 \
+    -audiodev ${AUDIO_DRIVER},id=snd0 \
+    -machine pcspk-audiodev=snd0 \
+    -S -gdb tcp::1234 -d int
+else
+  qemu-system-x86_64 \
+    -hda Image/x64BareBonesImage.qcow2 \
+    -m 512 \
+    -audiodev ${AUDIO_DRIVER},id=snd0 \
+    -machine pcspk-audiodev=snd0
 fi
