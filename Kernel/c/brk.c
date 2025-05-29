6ac8875602762e16f9563c619005f199a373fce6
torovtn <toribiovitonsconza@gmail.com>
2025-05-28 21:26:28 -0300

    changed types to match prev memory

--- Kernel/c/brk.c
+++ Kernel/c/brk.c
@@ -1,15 +1,17 @@
 #include <brk.h>
+#include <stdio.h>
+#include <stdint.h>
 
-#define HEAP_SIZE (1 << 20) // 1MB heap
+#define HEAP_SIZE (1 << 20) * 64 //64 * 1MB heap
 
-static uint8_t *heap_start;
-static uint8_t *heap_end;          // Current program break
-static uint8_t *heap_limit;
+static void *heap_start;
+static void *heap_end;          // Current program break
+static void *heap_limit;
 
-static const uint64_t addressByteSize = sizeof(void*);
+static const __uint64_t addressByteSize = sizeof(void*);
 
 void buddy_init(void * endOfModules){  
-    heap_start = (void*)(( (uint64_t) endOfModules + addressByteSize - 1) & ~(addressByteSize - 1));
+    heap_start = (void*)(( (__uint64_t) endOfModules + addressByteSize - 1) & ~(addressByteSize - 1));
     heap_end = heap_start;          // Current program break
     heap_limit = heap_start + HEAP_SIZE;  // Max limit  
 }
